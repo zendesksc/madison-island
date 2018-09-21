1dd6e4d1931cdce7a83d5be677ee49783727176a
Adam Gray <adam@pateo.co>
2018-09-21 11:30:32 +0100

    Remove all underscores from user field labels

--- src/containers/UserInfoContainer.js
+++ src/containers/UserInfoContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 function toTitleCase(str) {
-  return str.replace('_', ' ').replace(/\w\S*/g, function (txt) {
+  return str.replace(/_/g, ' ').replace(/\w\S*/g, function (txt) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   });
 }
